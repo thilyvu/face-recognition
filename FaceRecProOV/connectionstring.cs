38e2e7b61c5b1b189afd5973516bfb8d3f4c49af
thilyvu <thilyvu.2000@gmail.com>
2020-08-26 10:35:39 +0700

    first commit

---
+++ FaceRecProOV/connectionstring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiFaceRec
+{
+    class connectionstring
+    {
+       public static string Connectionstring = @"Data Source=LAPTOP-CBHLNUT7\SQLEXPRESS;Initial Catalog=nhandien;Integrated Security=True";
+    }
+}
